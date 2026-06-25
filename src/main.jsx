b10421be6351ef319d07bc1e7dc466a49a81da3a
sithlord2004 <amaini@icloud.com>
2026-06-25 10:07:54 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 import './styles.css'
-
-const updateSW = registerSW({
-  immediate: true,
-  onNeedRefresh() { updateSW(true) },
-  onRegisteredSW(_url, reg) {
-    if (reg) {
-      setInterval(() => reg.update(), 30 * 60 * 1000)
-      document.addEventListener('visibilitychange', () => { if (!document.hidden) reg.update() })
-    }
-  }
-})
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
